6c83b59ddc67e461e7a6469431f8e9373005ca95
Joe Hitchens <joe@sleepless.com>
2021-04-10 11:35:27 -0700

    -

--- index.js
+++ index.js
@@ -74,10 +74,10 @@ var mkLog = function(prefix) {
 		process.stdout.write("\n");
 		return logFunc
 	}
-	f.E = function(s) { f(1, `${style.red.open}******* ${s}${style.red.close}`); }	// error
-	f.W = function(s) { f(2, `${style.yellow.open}- - - - ${s}${style.yellow.close}`); }	// warning
-	f.I = function(s) { f(3, `${style.cyan.open}${s}${style.cyan.close}`); }				// info
-	f.V = function(s) { f(4, `${style.white.open}${s}${style.white.close}`); }				// verbose
+	f.E = function(s) { f(1, `${style.red.open}${s}${style.red.close}`); }	// error
+	f.W = function(s) { f(2, `${style.yellow.open}${s}${style.yellow.close}`); }	// warning
+	f.I = function(s) { f(3, `${s}`); }				// info
+	f.V = function(s) { f(4, `${style.cyan.open}${s}${style.cyan.close}`); }				// verbose
 	f.D = function(s) { f(5, `${style.magenta.open}${s}${style.magenta.close}`); }				// debug
 	return f;
 }
